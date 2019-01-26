d7c45e3cf34cc996fd34ddb40a802a8e5574f443
yadavshashwat <y.shashwat@gmail.com>
2019-01-26 07:24:55 +0530

    Correction

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -26,7 +26,6 @@ return (
 	         <li><a className="smoothscroll" href="#resume">Resume</a></li>
             <li><a className="smoothscroll" href="#portfolio">Works</a></li>
             <li><a className="smoothscroll" href="#testimonials">Testimonials</a></li>
-            <li><a className="smoothscroll" href="#contact">Contact</a></li>
          </ul>
 
       </nav>
